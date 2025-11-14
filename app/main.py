0ced211b07b214d2e7548c078d83b46ec6bf48c7
Timur Chermoshentsev <ctv2140@gmail.com>
2025-11-14 10:54:42 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from typing import Callable
+import functools
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+def cache(func: callable) -> callable:
+    cash_storage = {}
+
+    @functools.wraps(func)
+    def wrapper(*args: tuple) -> callable:
+        if args in cash_storage:
+            print("Getting from cash")
+            return cash_storage[args]
+
+        print("Calculating new result")
+        result = func(*args)
+        cash_storage[args] = result
+        return result
+    return wrapper
