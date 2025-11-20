dd288b6eaf30535068b1a66bb9e75394ed9f0b65
Timur Chermoshentsev <ctv2140@gmail.com>
2025-11-20 14:52:46 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,18 +2,17 @@ from typing import Callable
 import functools
 
 
-
 def cache(func: callable) -> callable:
-    cashe_storage = {}
+    cache_storage = {}
 
     @functools.wraps(func)
     def wrapper(*args: tuple) -> callable:
-        if args in cashe_storage:
+        if args in cache_storage:
             print("Getting from cashe")
-            return cashe_storage[args]
+            return cache_storage[args]
 
         print("Calculating new result")
         result = func(*args)
-        cashe_storage[args] = result
+        cache_storage[args] = result
         return result
     return wrapper
